4677fb99d86ec93bd0f7a2ff604358813f98006a
Ahmad10Raza <rjaahmad60@gmail.com>
2023-10-04 02:51:54 +0530

    Day_13

---
+++ Strings/Count_Binary_Sunstring.java
@@ -0,0 +1,42 @@
+public class Count_Binary_Sunstring {
+
+   public  static int maxSubStr(String str, int n)
+    {
+     
+        // To store the count of 0s and 1s
+        int count0 = 0, count1 = 0;
+     
+        // To store the count of maximum
+        // substrings str can be divided into
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (str.charAt(i) == '0')
+            {
+                count0++;
+            }
+            else
+            {
+                count1++;
+            }
+            if (count0 == count1)
+            {
+                cnt++;
+            }
+        }
+     
+        // It is not possible to
+        // split the string
+        if (count0 != count1)
+        {
+            return -1;
+        }
+        return cnt;
+    }
+     
+
+    public static void main(String[] args) {
+        String s = "0100110101";
+        System.out.println(maxSubStr(s, s.length()));
+    }
+}
