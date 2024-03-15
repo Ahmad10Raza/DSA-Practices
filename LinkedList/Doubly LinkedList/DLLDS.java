8894294984586a7fb057c8836f30945f669892b0
Ahmad10Raza <rjaahmad60@gmail.com>
2024-03-16 00:43:13 +0530

    Add reverseDLL method to DLLDS class

--- LinkedList/Doubly LinkedList/DLLDS.java
+++ LinkedList/Doubly LinkedList/DLLDS.java
@@ -158,6 +158,22 @@ public class DLLDS {
 
     }
 
+    public Node reverseDLL(Node head){
+        Node temp = null;
+        Node current = head;
+
+        while(current != null){
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        if(temp != null){
+            head = temp.prev;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         DLLDS dll = new DLLDS();
         dll.insertAtFront(30);
