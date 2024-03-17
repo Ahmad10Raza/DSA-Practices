a5b9df78783df4e12e3d2e11558baf248489c1f2
Ahmad10Raza <rjaahmad60@gmail.com>
2024-03-17 14:39:15 +0530

    Add multiplyTwoLL function to LLDS.java

--- LinkedList/LLDS.java
+++ LinkedList/LLDS.java
@@ -958,6 +958,48 @@ public class LLDS {
     }
 
 
+    // function to multiply two linked list
+     
+     public int multiplyTwoLL(Node l1, Node l2) {
+        int num1 = 0, num2 = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                num1 = num1 * 10 + l1.data;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                num2 = num2 * 10 + l2.data;
+                l2 = l2.next;
+            }
+        }
+        return num1 * num2;
+    }
+
+    // Multiply contents of two linked lists 
+    static long multiplyTwoLists(Node first, Node second) 
+    { 
+        long N = 1000000007; 
+        long num1 = 0, num2 = 0; 
+  
+        while (first != null || second !=  null){ 
+              
+            if(first != null){ 
+                num1 = ((num1)*10)%N + first.data; 
+                first = first.next; 
+            } 
+              
+            if(second != null) 
+            { 
+                num2 = ((num2)*10)%N + second.data; 
+                second = second.next; 
+            } 
+              
+        } 
+        return ((num1%N)*(num2%N))%N; 
+    } 
+  
+
+
     public static void main(String[] args) {
         LLDS list = new LLDS();
         list.insertAtEnd(1);
