bacd0832862005d416fecfb4ed0b9195ce80b57f
Ahmad10Raza <rjaahmad60@gmail.com>
2024-03-26 19:23:31 +0530

    Add methods for finding the inorder successor and printing nodes in a range

--- Binary_Search_Trees/BinarySearchTreesDS.java
+++ Binary_Search_Trees/BinarySearchTreesDS.java
@@ -1,4 +1,8 @@
-
+import java.util.*;
+    
+    
+    
+    
     public class BinarySearchTreesDS {
     class Node {
     int key;
@@ -99,7 +103,7 @@
             else if (root.right == null)
                 return root.left;
 
-            root.key = minValue(root.right);
+            root.key = inorderSuccessor(root.right);
 
             root.right = deleteRec(root.right, root.key);
         }
@@ -107,13 +111,86 @@
         return root;
     }
 
-    int minValue(Node root) {
+    // Find the inorder successor of the node
+
+    // Inorder successor is the leftmost node in the right subtree
+
+
+    // Tree:        50
+    //            /     \
+    //          30       70
+    //         /  \     /  \
+    //       20   40   60   80
+
+    // Inorder traversal of the tree: 20 30 40 50 60 70 80
+    // Inorder successor of 50: 60
+
+    int inorderSuccessor(Node root) {
         int minv = root.key;
         while (root.left != null) {
             minv = root.left.key;
             root = root.left;
         }
         return minv;
+    }
+
+
+    // Function to print node in range k1 and k2
+
+    // Tree:        50
+    //            /     \
+    //          30       70
+    //         /  \     /  \
+    //       20   40   60   80
+
+    // Input: k1 = 20, k2 = 70
+    // Output: 20 30 40 50 60 70
+
+    public void printInRange(Node root, int k1, int k2) {
+        if (root == null)
+            return;
+
+        if (k1 < root.key)
+            printInRange(root.left, k1, k2);
+
+        if (k1 <= root.key && k2 >= root.key)
+            System.out.print(root.key + " ");
+
+        if (k2 > root.key)
+            printInRange(root.right, k1, k2);
+    }
+
+
+    // Function to print node to root node path to leaf node
+
+    // Tree:        50
+    //            /     \
+    //          30       70
+    //         /  \     /  \
+    //       20   40   60   80
+
+    // Input: ~
+    // Output: 50 30 20
+    //         50 30 40
+    //         50 70 60
+    //         50 70 80
+
+    public void printRootToLeaf(Node root, List<Integer> path) {
+        if (root == null)
+            return;
+
+        path.add(root.key);
+
+        if (root.left == null && root.right == null) {
+            System.out.println(path);
+            path.remove(path.size() - 1);
+            return;
+        }
+
+        printRootToLeaf(root.left, path);
+        printRootToLeaf(root.right, path);
+
+        path.remove(path.size() - 1);
     }
 
 
@@ -132,15 +209,18 @@
         System.out.println("Inorder traversal of the BST:");
         bst.inorder();
 
-        int key = 60;
-        if (bst.search(key))
-            System.out.println("\nKey " + key + " found in the BST");
-        else
-            System.out.println("\nKey " + key + " not found in the BST");
+        // int key = 60;
+        // if (bst.search(key))
+        //     System.out.println("\nKey " + key + " found in the BST");
+        // else
+        //     System.out.println("\nKey " + key + " not found in the BST");
 
-        bst.delete(20);
-        System.out.println("\nInorder traversal after deleting 20:");
-        bst.inorder();
+        // bst.delete(20);
+        // System.out.println("\nInorder traversal after deleting 20:");
+        // bst.inorder();
+
+        System.out.println("\nFollowing Ouput is the nodes in the range K1 and K2:");
+        bst.printInRange(bst.root, 20, 70);
 
         
     }
