eaee66abf9a482a413c8efad7e3671c8523e03f5
Ahmad10Raza <rjaahmad60@gmail.com>
2024-04-09 23:36:40 +0530

    Refactor ArrayDS class to include method for finding the length of the longest consecutive subsequence in ArrayDS.java

--- Array/ArrayDS.java
+++ Array/ArrayDS.java
@@ -3,9 +3,8 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Stack;
-import java.util.Iterator;
+// import java.util.Iterator;
 import java.util.Set;
-import java.util.Iterator;
 
 
 
@@ -1587,10 +1586,65 @@ class Interval {
 
 
 
+    // Q_22: Find longest coinsecutive subsequence
+    // Given an array of integers, find the length of the longest sub-sequence 
+    // such that elements in the subsequence are consecutive integers, 
+    // the consecutive numbers can be in any order. 
 
+    // Examples:      
+    // Input: arr[] = {1, 9, 3, 10, 4, 20, 2}
+    // Output: 4
+    // Explanation: The subsequence 1, 3, 4, 2 is the longest subsequence of consecutive elements    
+    // Input: arr[] = {36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42}
+    // Output: 5
+    // Explanation: The subsequence 36, 35, 33, 34, 32 is the longest subsequence of consecutive elements.
+    
+    // Approach: Hashing
+    // T.C = O(n), S.C = O(n)
 
+    // Follow the steps below to solve the problem:
 
+    // Create an empty hash.
+    // Insert all array elements to hash.
+    // Do the following for every element arr[i]
+    // Check if this element is the starting point of a subsequence. To check this, 
+    //     simply look for arr[i] – 1 in the hash, if not found, then this is the first element of a subsequence.
+    // If this element is the first element, then count the number of elements in the 
+    //     consecutive starting with this element. Iterate from arr[i] + 1 till the last element that can be found.
+    // If the count is more than the previous longest subsequence found, then update this.
+    
 
+     // consecutive subsequence
+     public int findLongestConseqSubseq(int arr[], int n)
+     {
+         HashSet<Integer> S = new HashSet<Integer>();
+         int ans = 0;
+ 
+         // Hash all the array elements
+         for (int i = 0; i < n; ++i)
+             S.add(arr[i]);
+ 
+         // check each possible sequence from the start
+         // then update optimal length
+         for (int i = 0; i < n; ++i) {
+             // if current element is the starting
+             // element of a sequence
+             if (!S.contains(arr[i] - 1)) {
+                 // Then check for next elements
+                 // in the sequence
+                 int j = arr[i];
+                 while (S.contains(j))
+                     j++;
+ 
+                 // update  optimal length if this
+                 // length is more
+                 if (ans < j - arr[i])
+                     ans = j - arr[i];
+             }
+         }
+         return ans;
+     }
+ 
 
 
 
@@ -1740,8 +1794,14 @@ class Interval {
         // System.out.println(array.subArrayExists(arr, n));
 
 
-        int N = 100;
-        array.factorial(N);
+        // int N = 100;
+        // array.factorial(N);
+
+
+        int arr[] = {1, 9, 3, 10, 4, 20, 2};
+        int n = arr.length;
+        System.out.println(array.findLongestConseqSubseq(arr, n));
+
 
       
 
