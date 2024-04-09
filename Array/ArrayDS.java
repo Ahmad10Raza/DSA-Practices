b8c27cb4f7856fe696002ff48d4e735e4a555931
Ahmad10Raza <rjaahmad60@gmail.com>
2024-04-10 02:04:59 +0530

    Add solution for finding the smallest subarray with sum greater than a given value in ArrayDS.java

--- Array/ArrayDS.java
+++ Array/ArrayDS.java
@@ -2091,6 +2091,64 @@ class Interval {
   
 
 
+    // Q_28: Smallest Subarray with sum greater than a given value
+    // Given an array arr[] of integers and a number x, the task is to find the smallest 
+    // subarray with a sum greater than the given value. 
+
+    // Examples:
+    
+    // arr[] = {1, 4, 45, 6, 0, 19}
+    //    x  =  51
+    // Output: 3
+    // Minimum length subarray is {4, 45, 6}
+    // arr[] = {1, 10, 5, 2, 7}
+    //    x  = 9
+    // Output: 1
+    // Minimum length subarray is {10}
+    // arr[] = {1, 11, 100, 1, 0, 200, 3, 2, 1, 250}
+    //     x = 280
+    // Output: 4
+    // Minimum length subarray is {100, 1, 0, 200}
+    // arr[] = {1, 2, 4}
+    //     x = 8
+    // Output : Not Possible
+    // Whole array sum is smaller than 8.
+    
+    // Approach: Two-Pointer Technique
+    // T.C = O(n), S.C = O(1)
+
+    // The idea is to maintain a window that has a sum greater than the given value.
+    // We maintain a window by having two pointers, l and r.
+    // We keep incrementing r until the sum of the window is greater than the given value.
+    // Once the sum is greater, we increment l until the sum is less than the given value.
+    // We keep track of the minimum length of the window.
+    // The implementation of the above approach is as follows:
+
+    public int smallestSubWithSum(int arr[], int n, int x)
+    {
+        // Initialize current sum and minimum length
+        int curr_sum = 0, min_len = n + 1;
+ 
+        // Initialize starting and ending indexes
+        int start = 0, end = 0;
+        while (end < n) {
+            // Keep adding array elements while the current sum
+            // is smaller than or equal to x
+            while (curr_sum <= x && end < n)
+                curr_sum += arr[end++];
+ 
+            // If current sum becomes greater than x.
+            while (curr_sum > x && start < n) {
+                // Update minimum length if needed
+                if (end - start < min_len)
+                    min_len = end - start;
+ 
+                // remove starting elements
+                curr_sum -= arr[start++];
+            }
+        }
+        return min_len;
+    }
 
 
     public static void main(String[] args) {
@@ -2275,9 +2333,13 @@ class Interval {
         // int n = arr.length;
         // System.out.println(array.maxWater(arr, n));
         // System.out.println(array.findWater(arr, n));
+
+
+        // int arr[] = {1, 4, 45, 6, 0, 19};
+        // int n = arr.length;
+        // int x = 51;
+        // System.out.println(array.smallestSubWithSum(arr, n, x));
         
-
-
 
       
 
