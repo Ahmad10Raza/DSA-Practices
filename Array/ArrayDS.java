7b63b7e35eb65fae47ab048b541827c8277692ac
Ahmad10Raza <rjaahmad60@gmail.com>
2024-04-13 01:15:38 +0530

    Add solution for finding the minimum number of swaps required to bring elements less than or equal to k together in ArrayDS.java

--- Array/ArrayDS.java
+++ Array/ArrayDS.java
@@ -2151,6 +2151,74 @@ class Interval {
     }
 
 
+    // Q_29: Minimum swaps required bring elements less equal K together
+    // Given an array of n positive integers and a number k. Find the minimum number 
+    // of swaps required to bring all the numbers less than or equal to k together. 
+
+    // Example: 
+    
+    // Input:  arr[] = {2, 1, 5, 6, 3}, k = 3
+    // Output: 1
+    // Explanation: 
+    // To bring elements 2, 1, 3 together, swap element ‘5’ with ‘3’ such that final array will be arr[] = {2, 1, 3, 6, 5}
+    
+    // Input:  arr[] = {2, 7, 9, 5, 8, 7, 4}, k = 5
+    // Output: 2
+    
+    // Approach: Sliding Window Technique
+    // T.C = O(n), S.C = O(1)
+
+    // The idea is to count the number of elements greater than k.
+//     Find the count of all elements which are less than or equal to ‘k’. Let’s say the count is ‘cnt’
+// Using the two-pointer technique for a window of length ‘cnt’, each time keep track of how many 
+// elements in this range are greater than ‘k’. Let’s say the total count is ‘bad’.
+// Repeat step 2, for every window of length ‘cnt’ and take a minimum of count ‘bad’ among them. This will be the final answer.
+    // We maintain a window of size equal to the count of elements greater than k.
+    // We keep track of the minimum number of elements greater than k in the window.
+    // The implementation of the above approach is as follows:
+
+    public int minSwap(int arr[], int n, int k)
+    {
+        // Find count of elements which are less than
+        // equals to k
+        int count = 0;
+        for (int i = 0; i < n; ++i)
+            if (arr[i] <= k)
+                ++count;
+     
+        // Find unwanted elements in current window of
+        // size 'count'
+        int bad = 0;
+        for (int i = 0; i < count; ++i)
+            if (arr[i] > k)
+                ++bad;
+     
+        // Initialize answer with 'bad' value of
+        // current window
+        int ans = bad;
+        for (int i = 0, j = count; j < n; ++i, ++j) {
+     
+            // Decrement count of previous window
+            if (arr[i] > k)
+                --bad;
+     
+            // Increment count of current window
+            if (arr[j] > k)
+                ++bad;
+     
+            // Update ans if count of 'bad'
+            // is less in current window
+            ans = Math.min(ans, bad);
+        }
+        return ans;
+    }
+
+
+    
+
+
+
+
     public static void main(String[] args) {
         ArrayDS array = new ArrayDS(5);
         array.add(1);
@@ -2341,7 +2409,10 @@ class Interval {
         // System.out.println(array.smallestSubWithSum(arr, n, x));
         
 
-      
+        int arr[] = {1, 2, 3, 4, 5, 6, 3};
+        int n = arr.length;
+        int k = 3;
+        System.out.println(array.minSwap(arr, n, k));
 
 
 
