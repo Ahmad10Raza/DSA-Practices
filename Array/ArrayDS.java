9361e0b5e4c150ab3af05acfdbd4a732e9bcc404
Ahmad10Raza <rjaahmad60@gmail.com>
2024-04-13 01:22:54 +0530

    Add solution for finding the minimum number of operations required to make an array palindrome in ArrayDS.java

--- Array/ArrayDS.java
+++ Array/ArrayDS.java
@@ -2214,9 +2214,84 @@ class Interval {
     }
 
 
+    // Q_30: Minimum no. of operations required to make an array palindrome
+    // Given an array of positive integers. We need to make the given array a ‘Palindrome’. 
+    // The only allowed operation is”merging” (of two adjacent elements). 
+    // Merging two adjacent elements means replacing them with their sum. 
+    // The task is to find the minimum number of merge operations required to make the given array a ‘Palindrome’.
+
+    // To make any array a palindrome, we can simply apply merge operation n-1 times 
+    // where n is the size of the array (because a single-element array is always palindromic, 
+    // similar to single-character string). In that case, the size of array will be reduced to 1. 
+    // But in this problem, we are asked to do it in the minimum number of operations.
     
+    // Example : 
+    
+    // Input : arr[] = {15, 4, 15}
+    // Output : 0
+    // Array is already a palindrome. So we
+    // do not need any merge operation.
+    // Input : arr[] = {1, 4, 5, 1}
+    // Output : 1
+    // We can make given array palindrome with
+    // minimum one merging (merging 4 and 5 to
+    // make 9)
+    // Input : arr[] = {11, 14, 15, 99}
+    // Output : 3
+    // We need to merge all elements to make
+    // a palindrome.
+      
+    // Approach: Two-Pointer Technique
+    // T.C = O(n), S.C = O(1)
 
+    // Let f(i, j) be minimum merging operations to make subarray arr[i..j] a palindrome. 
+    // If i == j answer is 0. We start i from 0 and j from n-1.
 
+    // If arr[i] == arr[j], then there is no need to do any merging operations at index i or index j. 
+    // Our answer in this case will be f(i+1, j-1).
+    // Else, we need to do merging operations. Following cases arise.
+    // If arr[i] > arr[j], then we should do merging operation at index j. We merge index j-1 and j, 
+    // and update arr[j-1] = arr[j-1] + arr[j]. Our answer in this case will be 1 + f(i, j-1).
+    // For the case when arr[i] < arr[j], update arr[i+1] = arr[i+1] + arr[i]. Our answer in this case will be 1 + f(i+1, j).
+    // Our answer will be f(0, n-1), where n is the size of array arr[].
+    // Therefore this problem can be solved iteratively using two pointers (first pointer 
+    // pointing to start of the array and second pointer pointing to the last element 
+    // of the array) method and keeping count of total merging operations done till now.
+
+    public int findMinOps(int[] arr, int n)
+    {
+        int ans = 0; // Initialize result
+     
+        // Start from leftmost and rightmost corners of
+        // arr[]
+        for (int i = 0, j = n - 1; i <= j;) {
+     
+            // If corner elements are same, problem
+            // reduces arr[i+1..j-1]
+            if (arr[i] == arr[j]) {
+                i++;
+                j--;
+            }
+     
+            // If left element is greater, then we merge
+            // right two elements
+            else if (arr[i] > arr[j]) {
+                // need to merge from tail.
+                j--;
+                arr[j] += arr[j + 1];
+                ans++;
+            }
+     
+            // Else we merge left two elements
+            else {
+                i++;
+                arr[i] += arr[i - 1];
+                ans++;
+            }
+        }
+     
+        return ans;
+    }
 
 
     public static void main(String[] args) {
@@ -2409,10 +2484,17 @@ class Interval {
         // System.out.println(array.smallestSubWithSum(arr, n, x));
         
 
-        int arr[] = {1, 2, 3, 4, 5, 6, 3};
-        int n = arr.length;
-        int k = 3;
-        System.out.println(array.minSwap(arr, n, k));
+        // int arr[] = {1, 2, 3, 4, 5, 6, 3};
+        // int n = arr.length;
+        // int k = 3;
+        // System.out.println(array.minSwap(arr, n, k));
+
+
+        // int arr[] = {1, 4, 5, 1};
+        // int n = arr.length;
+        // System.out.println(array.findMinOps(arr, n));
+            
+
 
 
 
