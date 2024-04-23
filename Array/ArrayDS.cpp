d78b4566b63d9b26ec1bd514e1fefe92946db325
Ahmad10Raza <rjaahmad60@gmail.com>
2024-04-23 14:36:21 +0530

    Add reverseArray function to ArrayDS class in ArrayDS.cpp

--- Array/ArrayDS.cpp
+++ Array/ArrayDS.cpp
@@ -218,6 +218,19 @@ public:
         }
         return ans;
     }
+
+    // Function To reverse the array
+    void reverseArray(int arr[], int n){
+        int low = 0;
+        int high = n - 1;
+        while(low < high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
 };
 
 int main() {
