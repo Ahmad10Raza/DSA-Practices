d2058086bc7b6448fc09e95b50479d616aad6fde
Ahmad10Raza <rjaahmad60@gmail.com>
2024-05-02 16:57:45 +0530

    Added Bubble and Selection Sort

--- Searching & Sorting/Selection_Sort.java
+++ Searching & Sorting/Selection_Sort.java
@@ -1,3 +1,9 @@
+
+
+// The algorithm repeatedly selects the smallest (or largest) element from the unsorted 
+// portion of the list and swaps it with the first element of the unsorted part. 
+// This process is repeated for the remaining unsorted portion until the entire list is sorted.
+
 public class Selection_Sort
 {
     public static void selectionSort(int[] arr){
