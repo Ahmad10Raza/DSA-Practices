b3041e3aee11b663861ea70d7d52508c578f7541
Ahmad10Raza <rjaahmad60@gmail.com>
2024-05-02 17:18:14 +0530

    Added Insertion Sort

--- Searching & Sorting/Insertion_Sort.java
+++ Searching & Sorting/Insertion_Sort.java
@@ -1,3 +1,34 @@
+
+
+// Array: [23, 1, 10, 5, 2]
+// First Pass:
+// Current element is 23
+// The first element in the array is assumed to be sorted.
+// The sorted part until 0th index is : [23]
+
+// Second Pass:
+// Compare 1 with 23 (current element with the sorted part).
+// Since 1 is smaller, insert 1 before 23.
+// The sorted part until 1st index is: [1, 23]
+
+// Third Pass:
+// Compare 10 with 1 and 23 (current element with the sorted part).
+// Since 10 is greater than 1 and smaller than 23, insert 10 between 1 and 23.
+// The sorted part until 2nd index is: [1, 10, 23]
+
+// Fourth Pass:
+// Compare 5 with 1, 10, and 23 (current element with the sorted part).
+// Since 5 is greater than 1 and smaller than 10, insert 5 between 1 and 10.
+// The sorted part until 3rd index is: [1, 5, 10, 23]
+
+// Fifth Pass:
+// Compare 2 with 1, 5, 10, and 23 (current element with the sorted part).
+// Since 2 is smaller than all elements in the sorted part, insert 2 at the beginning.
+// The sorted part until 4th index is: [2, 1, 5, 10, 23]
+
+// Final Array:
+// The sorted array is: [2, 1, 5, 10, 23]
+
 public class Insertion_Sort {
     public static void insertionSort(int array[]) {
         int n = array.length;
